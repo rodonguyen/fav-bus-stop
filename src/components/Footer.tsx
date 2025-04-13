0d2002443c7e7b68c8f2a3a0fc7d28d73df848ef
Rodo <rodonguyendd@gmail.com>
2025-04-13 21:41:29 +1000

    feat: newer footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,21 +4,33 @@ import { kofiLogo } from '../assets';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-      <div>
-        <p>Made with ❤️ and hatred of lags in smart phone map apps</p>
-        <div className="flex items-center justify-center gap-2">
-          <a
-            href="https://ko-fi.com/rodo8888"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link link-hover flex items-center gap-1"
-          >
-            <img src={kofiLogo} alt="Kofi Logo" className="h-5 w-5" />
-            Buy me a matcha latte
-          </a>
-        </div>
-      </div>
+    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 lg:px-[14rem]">
+      <nav className="gap-y-4 flex flex-col">
+        <p className="text-neutral-content/60">Made with ❤️ and hatred of lags in smart phone map apps</p>
+        <a
+          className="link link-hover flex items-center gap-2"
+          href="https://ko-fi.com/rodo8888"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Buy me a matcha latte
+          <img src={kofiLogo} alt="Kofi Logo" className="h-5 w-5" />
+        </a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover" href="https://www.rodonguyen.dev" target="_blank" rel="noopener noreferrer">
+          About the author
+        </a>
+        <a className="link link-hover" href="https://www.beacons.ai/rodo8888" target="_blank" rel="noopener noreferrer">
+          Contact
+        </a>
+      </nav>
+      <nav className="text-neutral-content/40">
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+      </nav>
     </footer>
   );
 };
